6395fb838138c995627710a86fff70ee96f873e5
Arbab <mdnawaz0786@gmail.com>
2024-05-07 23:24:41 -0400

    Initial Commit

---
+++ MVVMDemo/MVVMDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMDemoApp.swift
+//  MVVMDemo
+//
+//  Created by Arbab Nawaz on 5/7/24.
+//
+
+import SwiftUI
+
+@main
+struct MVVMDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
